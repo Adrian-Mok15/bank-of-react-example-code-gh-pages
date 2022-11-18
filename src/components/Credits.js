dc67e2f7d9ebd680bfa237303a20d6206eb62f76
Adrian <mokadrian@yahoo.com>
2022-11-18 16:59:15 -0500

    adds to credit.js according to debit.js

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,13 +5,31 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import Debits from './Debits';
 
 const Credits = (props) => {
+   
+  let creditsView = () =>{
+    const {credits} = props;
+    return credits.map((credit) => {
+      let date = credit.date.slice(0,10);
+      return <l1 key={credit.id}>{credit.amount}{credit.description}{date}</l1>
+    })
+  }
+  
+  
   return (
     <div>
       <h1>Credits</h1>
       <br/>
       <Link to="/">Return to Home</Link>
+      {creditsView()}
+
+      <form onSubmit={props.addCredit}>
+        <input type="text" name="description" />
+        <input type="number" name="amount" />
+        <button type="submit">Add Credit</button>
+      </form>
     </div>
   )
 }
